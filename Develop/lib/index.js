f5d2f6004d0f5df1c93edf1256435e6a64e51f03
unknown <charles.pitek@gmail.com>
2020-01-18 13:01:11 -0800

    created manager class, passed all test, modules done for now

--- Develop/lib/index.js
+++ Develop/lib/index.js
@@ -1,3 +1,4 @@
 const Employee = require("../lib/Employee.js");
 const Engineer = require("../lib/Engineer.js");
 const Intern = require("../lib/Intern.js");
+const Manager = require("../lib/Manager.js");
